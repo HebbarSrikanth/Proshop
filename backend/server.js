8485199c1f2e7f94547369981ddabcc32badd250
Srikanth Hebbar M P <srikanth.hebbar14@outlook.com>
2020-10-20 00:44:27 +0530

    Completed till the 4 part

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 //Import the Express module using require keyword
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
-import dbConnect from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 dotenv.config()
 
+//Importing the Route files
+import productRoute from './routes/productRouter.js'
+
+//Importing the DB connect files and initiating the connection
+import dbConnect from './config/db.js'
 dbConnect()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 const environment = process.env.NODE_ENV
+app.listen(PORT, console.log(`Server is running in ${environment} mode in the port ${PORT}`.yellow.bold))
 
-//Fetch all the products present in the product file
-app.get('/products', (req, res) => {
-    res.json(products)
-})
+//Middleware Route for products
+//Note : In this next is not use as response will end the cycle that will be sent in Product router
+app.use('/api/products', productRoute)
 
-//Fetch individual products
-app.get('/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
-
-app.listen(PORT, console.log(`Server is running in ${environment} mode in the port ${PORT}`.yellow.bold)) 
+//Not Found Error
+app.use(notFound)
+//Error handling 
+app.use(errorHandler)
