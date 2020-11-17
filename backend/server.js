2c81ca131ea2f9745617caaec35ec9f8369e6a2f
Srikanth Hebbar M P <srikanth.hebbar14@outlook.com>
2020-11-17 23:49:40 +0530

    Prepare to build

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,15 @@ app.use('/api/upload', uploadRoute)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+}
+
+
+
 //Not Found Error
 app.use(notFound)
 //Error handling 
